1b744afa5eb0cf76d4459d24d0a3779bb4b7dd0b
grish <grishshrestha2@gmail.com>
2025-06-23 03:43:26 +0545

    Done CRUD in Food Items for Restaurant

--- Backend/src/main/java/com/food_rescue/backend/service/impl/FoodItemService.java
+++ Backend/src/main/java/com/food_rescue/backend/service/impl/FoodItemService.java
@@ -1,11 +1,12 @@
 package com.food_rescue.backend.service.impl;
 
 import com.food_rescue.backend.dto.FoodItemDTO;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface FoodItemService {
-    boolean createFoodItem(FoodItemDTO foodItemDTO);
+    boolean createFoodItem(FoodItemDTO foodItemDTO, MultipartFile file);
     boolean updateFoodItem(FoodItemDTO foodItemDTO);
     List<FoodItemDTO> getAllFoodItems();
     FoodItemDTO getFoodItemById(Long id);
